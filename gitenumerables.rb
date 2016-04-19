af0f6860c7330e59adfe98862c9bccfefdf7f66a
arlequine <arlequine666.ah@gmail.com>
2016-04-19 17:27:38 -0500

    cuatro trues

--- gitenumerables.rb
+++ gitenumerables.rb
@@ -12,17 +12,22 @@ end
 # Dado un array de números regresa un array con solo sol numeros impares
 # Utiliza: Enumerable#select
 def odd_integers(array)
+  array.select { |num| num.odd? }
 end
 
 # Dado un array y un límite regresa el primer numero que sea menor al límite 
 # Utiliza: Enumerable#find
 def first_under(array, limit)
+  array.find {|num| num < limit }
+  
 end
 
 
 # Dado un array de strings y regresa un nuevo array donde todos los elementos contengan al final un signo de admiración. 
 # Utiliza: Enumerable#map
 def add_bang(array)
+  exclamation = "!"
+  array.map { |word| word + exclamation }
 end
 
 # Dado un array de números calcula la suma de todos sus elementos. 
@@ -48,6 +53,7 @@ puts odd_integers([3, 4, 7, 9, 10, 16]) == [3, 7, 9]
 puts first_under([13, 21, 7, 0, 11, 106], 10) == 7
 
 puts add_bang(["hi", "mom"]) == ["hi!", "mom!"]
+
 
 puts sum([1, 1, 2, 3, 5]) == 12
 
